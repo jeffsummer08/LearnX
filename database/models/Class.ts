6a9a80311d238fa843014433c5726b6281733752
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-09-07 12:52:39 -0500

    database setup

--- database/models/Class.ts
+++ database/models/Class.ts
@@ -1,1 +1,14 @@
 import { Generated, Selectable, Insertable, Updateable } from "kysely"
+
+export interface ClassTable {
+    ClassId: Generated<number>
+    Name: string
+    TeacherId: number
+    Students: number[]
+    Courses: number[]
+    ClassCode: string
+}
+
+export type Class = Selectable<ClassTable>
+export type NewClass = Insertable<ClassTable>
+export type UpdateClass = Updateable<ClassTable>
--- database/models/Teacher.ts
+++ database/models/Teacher.ts
@@ -1,1 +1,15 @@
 import { Generated, Selectable, Insertable, Updateable } from "kysely"
+
+export interface TeacherTable {
+    AccountId: Generated<number>
+    FirstName: string
+    LastName: string
+    Email: string
+    PasswordHash: string
+    Salt: string
+    ClassIds: number[]
+}
+
+export type Teacher = Selectable<TeacherTable>
+export type NewTeacher = Insertable<TeacherTable>
+export type TeacherUpdate = Updateable<TeacherTable>
