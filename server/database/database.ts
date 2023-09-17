5c86d210f4424f2263bf167a61a0d4ae3cbc2840
Mihir Sood <soodmihir18@gmail.com>
2023-09-16 23:32:24 -0500

    db settings are in

--- server/database/database.ts
+++ server/database/database.ts
@@ -10,7 +10,12 @@ interface Database {
 const db = new Kysely<Database>({
     dialect: new PostgresDialect({
         pool: new Pool({
-            connectionString: process.env.DATABASE_URL,
+            database: 'ogzxfpvy',
+            host: 'bubble.db.elephantsql.com',
+            user: 'ogzxfpvy',
+            passowrd: '1tjh3l6XGPAtGiWQQNijFU1SB8CPowoG',
+            port: 5432,
+            max: 5, //elephantsql allows 5 max for free tier
         })
     }),
 })
