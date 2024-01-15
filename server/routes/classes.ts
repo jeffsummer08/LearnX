d642ebcd61267daf0ac6e5e4b3670a9af755ab94
Mihir Sood <soodmihir18@gmail.com>
2024-01-14 20:51:15 -0600

    can get detailed user view

--- server/routes/classes.ts
+++ server/routes/classes.ts
@@ -94,9 +94,22 @@ router.get("/:join_code/view/:student_id", async (req: Request, res: Response) =
         })
     }
     else{
-        const progressQuery = await db.selectFrom("users").where("id", "=", parseInt(req.params.student_id)).innerJoin("progress", "users.id", "progress.userId").selectAll().execute()
+        const progressQuery = await db.selectFrom("users").where("id", "=", parseInt(req.params.student_id))
+            .innerJoin("progress as p", "users.id", "p.userId")
+            .innerJoin("courses as c", "p.courseId", "c.id")
+            .innerJoin("lessons as l", "p.lessonId", "l.id")
+            .innerJoin("units as u", "p.unitId", "u.id")
+            .select(["users.firstname", "users.lastname", "c.url as course_url", "u.url as unit_url", "l.url as lesson_url", "p.progress", "p.timestampCreated"]).execute()
         res.json({
-            student: progressQuery[0].firstname + " " + progressQuery[0].lastname
+            student: progressQuery[0].firstname + " " + progressQuery[0].lastname,
+            history: progressQuery.map(val => ({
+                student: val.firstname + " " + val.lastname,
+                course_url: val.course_url,
+                unit_url: val.unit_url,
+                lesson_url: val.lesson_url,
+                progress: val.progress,
+                timestamp: val.timestampCreated
+            }))
         })
     }
 })
